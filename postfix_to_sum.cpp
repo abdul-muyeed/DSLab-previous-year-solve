f48c8da37cbdeb28ca1278a686ec7900c16c541a
Abdul Muyeed <abdulmuyeed47@gmail.com>
2023-08-16 18:18:07 +0600

    dsa solve

---
+++ postfix_to_sum.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void init(){
+    freopen("input.txt","r",stdin);
+}
+int main(){
+    init();
+    string s;
+    cin>>s;
+    cout<<s<<endl;
+    
+}
