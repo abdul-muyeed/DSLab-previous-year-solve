1629247a1d08d040c302a5a19af67f16d126bc1a
Abdul Muyeed <abdulmuyeed47@gmail.com>
2023-08-17 11:41:02 +0600

    question 1 solved

--- postfix_to_sum.cpp
+++ postfix_to_sum.cpp
@@ -1,11 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool isOparator(char a){
+    return a == '+' || a == '-' || a == '/' || a == '*' || a=='^' ;
+}
+
+int infixtoSum(string &str){
+    stack<int> s;
+    for(char c:str){
+        // cout<<s.top()<<endl;
+        if(isOparator(c)){
+            int b =s.top();
+            s.pop();
+            int a =s.top();
+            s.pop();
+            int k;
+            if(c=='+'){
+                k=a+b;
+            }else if(c=='-'){
+                k=a-b;
+            }else if(c=='/'){
+                k=a/b;
+            }else if(c=='^'){
+                k = (int) pow(a,b);
+            }else{
+                k=a*b;
+            }
+            s.push(k);
+
+        }else{
+            if(c=='a'){
+                s.push(3);
+            }else if(c=='b'){
+                s.push(2);
+            }else if(c=='c'){
+                s.push(1);
+            }else{
+                s.push(c-'0');
+            }
+            
+        }
+    }
+    return s.top();
+}
 
 void init(){
     freopen("input.txt","r",stdin);
 }
 int main(){
     init();
+    string str;
+    cin>>str;
+    int sum = infixtoSum(str);
+    cout<<sum<<endl;
     
     
 }
